0d414bdea6832584d15f6e550238eb34c19fd7d7
Jonathan Hale <jdizzle@wireless-10-104-42-206.umd.edu>
2024-01-08 12:09:46 -0500

    added note randomizer todo timed delay

--- rusty-note/src/main.rs
+++ rusty-note/src/main.rs
@@ -1,4 +1,5 @@
 use hound::{WavWriter, SampleFormat};
+use rand::Rng;
 
 fn main() -> Result<(), hound::Error> {
     let file_spec = hound::WavSpec {
@@ -7,16 +8,19 @@ fn main() -> Result<(), hound::Error> {
         bits_per_sample: 16,
         sample_format: SampleFormat::Int
     };
-
+    
     let mut wrtr = WavWriter::create("sample.wav", file_spec)?;
 
     let amplitude = i16::MAX as f32;
     let frequency = 440.0;
     let duration_secs = 20;
     let sample_rate = file_spec.sample_rate as f32;
+    
+    let mut rng = rand::thread_rng();
+    let random_height: f32 = rng.gen_range(-1.0..1.5);
 
     for time in 0..(sample_rate as usize * duration_secs) {
-        let sample = (amplitude * (2.5 * std::f32::consts::PI * frequency * (time as f32) / sample_rate).sin()) as i16;
+        let sample = (amplitude * (random_height * std::f32::consts::PI * frequency * (time as f32) / sample_rate).sin()) as i16;
         wrtr.write_sample(sample)?;
     }
 
